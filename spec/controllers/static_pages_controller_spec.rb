7c6bdaec03ef399a1de4aff7b424af004ba99576
NilMV <nil.medvedev17@gmail.com>
2017-12-13 01:28:17 +0300

    config tests, add static pages + tests

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,19 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  render_views
+  Capybara.ignore_hidden_elements = false
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App\n"
+  end
 
-  describe "GET #home" do
-    it "returns http success" do
-      get :home
-      expect(response).to have_http_status(:success)
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'home'
+      response.body.should have_xpath("//title",:text => "\nHome\n| #{@base_title}")
     end
   end
 
-  describe "GET #help" do
-    it "returns http success" do
-      get :help
-      expect(response).to have_http_status(:success)
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'help'
+      response.body.should have_xpath("//title",:text => "\nHelp\n| #{@base_title}")
     end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'about'
+      response.body.should have_xpath("//title",:text => "\nAbout\n| #{@base_title}")
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,10 +13,14 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-RSpec.configure do |config|
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'capybara/rails' #######
 
+RSpec.configure do |config|
+  config.include Capybara::DSL  
   config.default_formatter = "doc"
-  
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
